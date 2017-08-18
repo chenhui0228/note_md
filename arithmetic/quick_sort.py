c066b4ea478f91ba15824f786f096494a058df85
码码要洗手 <forget_tian@sina.com>
2017-08-18 15:47:13 +0800

    add quick_sort.py

---
+++ arithmetic/quick_sort.py
@@ -0,0 +1,35 @@
+#coding: utf-8
+
+def quick_sort(arr=None, left=0, right=0):
+    if arr is None:
+        arr = []
+    if left >= right:
+        return
+    i = left
+    j = right
+    print('i: %s, j: %s' % (i, j))
+    old = arr[i]
+    while(i<j):
+        while(i<j and arr[j]>old):
+            j=j-1
+        if(i<j):
+            arr[i] = arr[j]
+            i=i+1
+        while(i<j and arr[i]<old):
+            i=i+1
+        if(i<j):
+            arr[j] = arr[i]
+            j=j-1
+
+    print('iiiiiiiiiiii: %s' % i)
+    arr[i] = old
+    print('sorting: %s' % arr)
+    quick_sort(arr, left, i-1)
+    quick_sort(arr, i+1, right)
+
+if __name__ == '__main__':
+    # arr = [4, 3, 5, 9, 2, 7, 8, 1, 6]
+    arr = [1,2,3,4,5,6]
+    print('old: %s' % arr)
+    quick_sort(arr, 0, len(arr)-1)
+    print('new: %s' % arr)
