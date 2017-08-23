92b0552f28be3285e3346125794461cd71770825
码码要洗手 <forget_tian@sina.com>
2017-08-23 10:24:05 +0800

    update ex_headsort.py

--- arithmetic/ex_headsort.py
+++ arithmetic/ex_headsort.py
@@ -7,19 +7,20 @@ def adjustheap(arr=None, i=0, nlength=0):
     while(j < nlength):
         if(j+1<nlength and arr[j+1]>arr[j]):
             j += 1
-        if(arr[j] > temp):
+        if(arr[j] < temp):
             break
         arr[i] = arr[j]
         i = j
         j = i*2 + 1
     arr[i] = temp
 
-def heapsort(arr=None, root=0, nlength=0):
-    for i in range(nlength/2-1, 0, -1):
+def heapsort(arr=None, i=0, nlength=0):
+    for i in range(nlength/2-1, -1, -1):
         adjustheap(arr, i, nlength)
+    print('init: %s' % arr)
     for i in range(nlength-1, 0, -1):
         arr[0], arr[i] = arr[i], arr[0]
-        adjustheap(arr, 0, i-1)
+        adjustheap(arr, 0, i)
 
 def main():
     arr = [2, 4, 1, 3, 8, 9, 10, 7, 6, 5]
