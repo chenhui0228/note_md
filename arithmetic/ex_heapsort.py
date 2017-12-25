42fcc8322380874c5e9944c9f7440291b3cb32b7
haijunt <haijunt@princetechs.com>
2017-12-25 20:38:43 +0800

    update heapsort*.py

--- arithmetic/ex_heapsort.py
+++ arithmetic/ex_heapsort.py
@@ -5,7 +5,7 @@ def adjustheap(arr=None, i=0, nlength=0):
     j = i*2 + 1
     temp = arr[i]
     while(j < nlength):
-        if(j+1<nlength and arr[j+1]>arr[j]):
+        if(j+1 < nlength and arr[j+1] > arr[j]):
             j += 1
         if(arr[j] < temp):
             break
@@ -14,6 +14,7 @@ def adjustheap(arr=None, i=0, nlength=0):
         j = i*2 + 1
     arr[i] = temp
 
+
 def heapsort(arr=None, i=0, nlength=0):
     for i in range(nlength/2-1, -1, -1):
         adjustheap(arr, i, nlength)
@@ -21,6 +22,7 @@ def heapsort(arr=None, i=0, nlength=0):
     for i in range(nlength-1, 0, -1):
         arr[0], arr[i] = arr[i], arr[0]
         adjustheap(arr, 0, i)
+
 
 def main():
     arr = [2, 4, 1, 3, 8, 9, 10, 7, 6, 5]
